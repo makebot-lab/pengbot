51c32d8ded8ad8d3bf72f0f847bb90fd81470022
WONDAEHAN <62107740+makebot-maker@users.noreply.github.com>
2020-11-25 17:33:14 +0900

    Create Pengbot.h

---
+++ library/Pengbot/Pengbot.h
@@ -0,0 +1,34 @@
+/* makebot pengbot library
+ * makebot homepage(English) : http://makebot.mystrikingly.com/
+ * makebot homepage(Korean) : https://makebot.modoo.at
+ * files : https://github.com/makebot-lab/pengbot
+ * made by WONDAEHAN.
+ */
+#ifndef Pengbot_h
+#define Pengbot_h
+
+#include <Servo.h>
+#include <Oscillator.h>
+#include <US.h>
+#include "Arduino.h"
+
+class Pengbot {
+  public:
+    void ready();
+    void attachservo(int x);
+    void detachservo(int x);
+    float distance();
+    void stop();
+    void forward(int speed);
+    void backward(int speed);
+    void turnleft(int speed);
+    void turnright(int speed);
+    void setleft(int angle);
+    void setright(int angle);
+    void sound(int soundnum);
+  private:
+    Oscillator servo[2];
+    US us;
+};
+
+#endif
